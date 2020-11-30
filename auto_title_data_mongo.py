74db14ee5c83e01fccd3c06b5710b75b82f6e42e
Terry chan <napoler2008@gmail.com>
2020-12-01 00:34:47 +0800

    auto更新文档

--- auto_title_data_mongo.py
+++ auto_title_data_mongo.py
@@ -5,24 +5,52 @@ import pymongo
 client = pymongo.MongoClient("localhost", 27017)
 DB = client.gpt2Write
 
-# 以写的方式打开文件，如果文件不存在，就会自动创建
-f= open("data/title.txt", 'w')
-n=0
+
+
+items={}
 for i, it in  enumerate(DB.content_pet.find({})):
 
     
     # kw=it['entity']+it['value']
     # if len(it['data'])>1:
-    if "|" in it["title"] or "-" in it["title"] or "_" in it["title"] or "|" in it["title"] or "~" in it["title"]:
-        n=n+1
-        print("-"*20)
-        print(it["title"])
-        f.writelines(it['title'])
-        f.writelines("\n")
-        f.writelines("\n")
-        if n%20==0 and n !=0:
-            name=f"data/title{n}.txt"
-            f= open(name, 'w')
+    # print(it["title"])
+    # if ("|" in it["title"] or "-" in it["title"] or "_" in it["title"] or "|" in it["title"] or "~" in it["title"] )and it["title"] not in items:
+    if "|" in it["title"] or "-" in it["title"] or "_" in it["title"] or "|" in it["title"] or "~" in it["title"] :
+        # print(it["title"])
+        print(i)
+        # if  it["title"] not in items:
+        # items.append(it["title"])
+        items[it["title"]]=0
+print(len(items))
+# items=list(set(items))
+# print(len(items))
+# 以写的方式打开文件，如果文件不存在，就会自动创建
+
+# os.makedirs("data/t01")
+dirnum=100
+for i in range(dirnum):
+    try:
+        os.makedirs(f"data/{i}")
+        pass
+    except:
+        pass
+    
+
+for n,item in enumerate( items.keys()):
+    
+    if n%10==0:
+        fn=n%dirnum
+        name=f"data/{fn}/title{n}.txt"
+        f= open(name, 'w')
+    n=n+1
+    print("-"*20)
+    print(item)
+    f.writelines(item)
+    f.writelines("\n")
+    f.writelines("\n")
+
+
+        
 f.close()
 
 # for i,resp in enumerate( scanResp):
